d4464d920e920efcef1b8db430a579dcaca042e6
Md.Mehedi Hasan <61658463+AyonIslam12@users.noreply.github.com>
2021-08-23 12:16:44 +0600

    Initilize the project

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <button @click.prevent="getBackendRequest()">Request Api</button>
+  <h1 v-if="apiValue" class="font-bold text-3xl">check: {{apiValue}}</h1>
+   <Tutorial/>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      apiValue: '',
+    }
+
+  },
+  mounted() {
+
+
+  },
+  methods: {
+    getBackendRequest(){
+      this.$axios.get('/test').then(res => {
+       this.apiValue = res.data;
+      })
+    }
+  }
+}
+</script>
